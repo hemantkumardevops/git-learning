511f61c765013a83b77910f3625f94af029b2a98
Hemant kumar <hemantkumardevops@gmail.com>
2026-06-11 22:05:10 +0530

    storage code added

--- terraform.tf
+++ terraform.tf
@@ -2,3 +2,11 @@ resource "azurerm_resource_group" "name" {
     name     = "rg-terraform"
     location = "East US"
   }
+
+resource "azurerm_storage_account" "name" {
+    name                     = "stterraform"
+    resource_group_name      = azurerm_resource_group.name.name
+    location                 = azurerm_resource_group.name.location
+    account_tier             = "Standard"
+    account_replication_type = "LRS"
+  }
